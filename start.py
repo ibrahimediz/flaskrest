8ee322cfaeb2ebdbea03759d1353ce3576ff9a51
İbrahim EDİZ <edizdesign@gmail.com>
2022-01-30 07:13:03 +0000

    ilk

---
+++ start.py
@@ -0,0 +1,11 @@
+liste = ["ilhanmert","canersoy","efecan",
+"mustafaismail","ayseberna","enes",
+"hasan","ege","nedimcan","ahmetbaran",
+"fahrettinorkun","celalbugra","onur","musa",
+"alibaris","ardacem","irem","seyit","bilal",
+"abdullah","ali","furkan","fatihcan"]
+import os
+
+
+for item in liste:
+    os.mkdir(item)
